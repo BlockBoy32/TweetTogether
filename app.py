6f6118086aeb24f44c37560e1f2cdc3bb6829e08
Anthony <anthony.katwan1@gmail.com>
2023-08-22 18:55:40 -0700

    feed

--- app.py
+++ app.py
@@ -24,6 +24,9 @@ BASE_URL = "https://api.twitter.com/2"
 def index():
     return render_template('index.html')
 
+
+
+    ##------------Posting 
 @app.route('/post_tweet', methods=['POST'])
 def post_tweet():
     tweet_text = request.form.get('tweet')
@@ -44,6 +47,26 @@ def post_tweet():
         if "errors" in data and len(data["errors"]) > 0:
             error_message = data["errors"][0].get("message", "Unknown error")
         return jsonify(status="error", message=error_message)
+    
+
+
+    ##------------FEED
+@app.route('/get_recent_tweets')
+def get_recent_tweets():
+    response = requests.get(
+        "https://api.twitter.com/1.1/statuses/user_timeline.json?count=10",
+        auth=auth
+    )
+    data = response.json()
+    if response.status_code == 200:
+        tweets = [{"id": tweet["id_str"], "text": tweet["text"]} for tweet in data]
+        return jsonify(tweets)
+    else:
+        return jsonify([])  # Return an empty list in case of an error
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
